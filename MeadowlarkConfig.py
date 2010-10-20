56f4fb4753d8351e6a60cec4c3857372014685b6
Zach Denton <zacharydenton@gmail.com>
2010-10-20 15:31:48 -0400

    changed name to Meadowlark

---
+++ MeadowlarkConfig.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Ship Configuration
+ship_base_health	= 100
+ship_base_speed		= 2.5
+
+# Weapon Configuration
+weapon_delay			= 0.3
+
+# Ammunition Configuration
+ammo_base_damage	= 10
+ammo_base_speed		= 3
+
+# Enemy Configuration
+max_num_enemies		= 30
+enemy_base_health	= 10
+enemy_base_speed	= 1.0
+enemy_base_skill	= 20.0 / 60.0
+
+# Powerup Configuration
+powerup_base_speed= 2
+powerup_base_value= 250
+powerup_chance 		= 5.0
+
+# Boss Configuration
+boss_base_health	= 200
+boss_base_speed		= 2.0
+boss_base_skill		= 40.0 / 60.0
